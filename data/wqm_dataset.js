3da754720eb11cfa0b84db6a3efcaf9a90ebe782
Dylan-Caminos <dylanterrell@Dylans-MacBook.local>
2017-04-19 11:42:19 -0500

    Revert "Test"
    This reverts commit a59dcfe1aee7db626fe5e254af094e735939dfc3.

--- data/wqm_dataset.js
+++ data/wqm_dataset.js
@@ -113,7 +113,7 @@ var WQM_MAP_DATA =
     "names_of_other_communities": "",
     "type": "",
     "orginazation_testing": "",
-    "community_name": “TEST Los Lopez",
+    "community_name": "Los Lopez",
     "fluoride": 2.44,
     "no3_ppm": "",
     "ph": "",
