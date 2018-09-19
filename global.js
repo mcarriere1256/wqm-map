57c4fc6421caac10f97ecba2c933fca0b7acff16
ask53 <askrupp@gmail.com>
2018-09-19 12:45:44 -0500

    global comments update

--- global.js
+++ global.js
@@ -120,6 +120,6 @@ var HELP_URL = "https://caminosdeagua.github.io/wqm-map/img/questionMark2.png";
 var HELP_URL_HOVER = "https://caminosdeagua.github.io/wqm-map/img/questionMark2Hover.png"
 
 var MAPBOX_ID = "pk.eyJ1IjoiY2FtaW5vc2RlYWd1YSIsImEiOiJjaXRsb3V1MDUwMGR2Mm9tdThncnYydTU4In0.DcxDn8J5RJ--WQ3gckONIw";
-var MAPBOX_MAP_TYPE = "caminosdeagua.935ff2fb";	// Which mapbox tile set to display. options: "mapbox.streets" (this one is the classic), "caminosdeagua.935ff2fb" (this displays small towns one zoom level earlier)		
+var MAPBOX_MAP_TYPE = "caminosdeagua.935ff2fb";	// Which mapbox tile set to display. options: "mapbox.streets" (classic), "caminosdeagua.935ff2fb" (displays small towns one zoom level earlier)		
 	
 var MAX_SEARCH_LENGTH = 6; 
