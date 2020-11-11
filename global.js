71106787330e8def46fb7c24233ba5c627da3eaa
mcarriere1256 <73039262+mcarriere1256@users.noreply.github.com>
2020-11-11 08:31:31 -0600

    Update global.js
    added points_valid to optimize pointIsValid() use in code.js

--- global.js
+++ global.js
@@ -57,6 +57,7 @@ var activeContaminant = -1; 			// A value that indicates the current contaminant
 
 var base;	 							// Store all info relevant to base points		
 var dup_indices;						// An array of arrays of the data indices of duplicate points.
+var points_valid;                                                // store the results of pointIsValid() for each entry in AllData
 										// 	Each internal array holds points with the same latLng. 
 var AllData;							// Global var to hold all data.
 var spiderFeatures; 					// a global var to store all of the data that's being spidered
